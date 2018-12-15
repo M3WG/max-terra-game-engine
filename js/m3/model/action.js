0efd8b298f07f0511a455e1045b79e5ebfb01bda
nicross <nicross@users.noreply.github.com>
2018-12-14 18:05:58 -0600

    Define basic action fields

--- js/m3/model/action.js
+++ js/m3/model/action.js
@@ -12,12 +12,26 @@ m3.model.action.prototype = (
 
     function construct(options) {
       this.turn = options.turn;
+      this.tile = options.tile;
+      this.x = options.x;
+      this.y = options.y;
+
+      _swapTile.call(this);
 
       return this;
     }
 
     function destruct() {
       return this;
+    }
+
+    // XXX: Not the time or place for this
+    // TODO: Good luck future us
+    function _swapTile() {
+      const map = this.turn.round.game.map;
+      const cell = map.getCell(this.x, this.y);
+
+      cell.tile = this.tile;
     }
 
     return {
