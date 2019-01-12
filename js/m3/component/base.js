9bdcdb54d5f346a57ab73ef2b3a6a9bc843583fa
nicross <nicross@users.noreply.github.com>
2019-01-11 19:43:23 -0600

    Create m3.component.base

---
+++ js/m3/component/base.js
@@ -0,0 +1,46 @@
+'use strict'
+
+m3.component.base = {}
+
+m3.component.base.prototype = (
+  function prototypeIIFE() {
+
+    function construct(config, parentElement, parentComponent) {
+      this.config = Object.assign({}, config)
+      this._parentElement = parentElement
+      this.parent = parentComponent
+
+      return this
+    }
+
+    function destruct() {
+      return this
+    }
+
+    function attach(parentElement) {
+      if (parentElement) {
+        this._parentElement = parentElement
+      }
+
+      if (this._parentElement) {
+        this._parentElement.appendChild(this._rootElement)
+      }
+
+      return this
+    }
+
+    function detach() {
+      if (this._rootElement) {
+        this._rootElement.remove()
+      }
+
+      return this
+    }
+
+    return {
+      construct,
+      destruct,
+      attach,
+    };
+  }
+)();
