4a4e00cfc090b3d944f0fc4a6a9fad11b47d8cab
nicross <nicross@users.noreply.github.com>
2019-01-18 20:07:25 -0600

    Create m3.model.base

---
+++ js/m3/model/base.js
@@ -0,0 +1,16 @@
+'use strict'
+
+m3.model.base = {}
+
+m3.model.base.prototype = (
+  function prototypeIIFE(undefined) {
+
+    function construct(options) {
+      return this
+    }
+
+    return {
+      construct,
+    }
+  }
+)()
