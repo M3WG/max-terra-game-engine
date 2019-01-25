df4de6eb853a25f08797e2d171c25f09a96a88f7
nicross <nicross@users.noreply.github.com>
2019-01-24 19:30:25 -0600

    Use hash within m3.config.tiles

--- js/m3/config/tiles.js
+++ js/m3/config/tiles.js
@@ -1,25 +1,29 @@
 'use strict'
 
-m3.config.tiles = [
-  undefined,
-  {
-    color: 'white',
+m3.config.tiles = {
+  1: {
+    id: 1,
+    color: '#CCCCCC',
     desc: 'city',
   },
-  {
+  2: {
+    id: 2,
     color: 'blue',
     desc: 'water'
   },
-  {
+  3: {
+    id: 3,
     color: 'yellow',
     desc: 'field'
   },
-  {
+  4: {
+    id: 4,
     color: 'green',
     desc: 'forest'
   },
-  {
-    color: 'black',
+  5: {
+    id: 5,
+    color: '#333333',
     desc: 'mountain'
   },
-]
+}
