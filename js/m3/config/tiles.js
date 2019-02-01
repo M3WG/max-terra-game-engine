f0bee3a6cb3f8a760a44028585745dec92dcba42
Moses <moses.wolfenstein@gmail.com>
2019-02-01 13:47:37 -0600

    Changed tile colors in config.

--- js/m3/config/tiles.js
+++ js/m3/config/tiles.js
@@ -3,27 +3,27 @@
 m3.config.tiles = {
   1: {
     id: 1,
-    color: '#CCCCCC',
+    color: '#cecac6',
     desc: 'city',
   },
   2: {
     id: 2,
-    color: 'blue',
+    color: '#4b3bf9',
     desc: 'water'
   },
   3: {
     id: 3,
-    color: 'yellow',
+    color: '#fff351',
     desc: 'field'
   },
   4: {
     id: 4,
-    color: 'green',
+    color: '#229316',
     desc: 'forest'
   },
   5: {
     id: 5,
-    color: '#333333',
+    color: '#382d2d',
     desc: 'mountain'
   },
 }
