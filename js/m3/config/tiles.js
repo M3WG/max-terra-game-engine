e4b95500d7cc398d7171ad6ee3fd33a6deec2bee
Moses <moses.wolfenstein@gmail.com>
2019-02-01 13:49:49 -0600

    Update tiles.js

--- js/m3/config/tiles.js
+++ js/m3/config/tiles.js
@@ -3,27 +3,27 @@
 m3.config.tiles = {
   1: {
     id: 1,
-    color: '#cecac6',
+    color: '#ced1c0',
     desc: 'city',
   },
   2: {
     id: 2,
-    color: '#4b3bf9',
+    color: '#4146db',
     desc: 'water'
   },
   3: {
     id: 3,
-    color: '#fff351',
+    color: '#edde6f',
     desc: 'field'
   },
   4: {
     id: 4,
-    color: '#229316',
+    color: '#2d873e',
     desc: 'forest'
   },
   5: {
     id: 5,
-    color: '#382d2d',
+    color: '#38262b',
     desc: 'mountain'
   },
 }
