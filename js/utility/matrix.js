cb79253bfbd359fa0f3032186039161e6f63741e
nicross <nicross@users.noreply.github.com>
2019-02-15 18:09:32 -0600

    Create utility.matrix

---
+++ js/utility/matrix.js
@@ -0,0 +1,35 @@
+'use strict'
+
+utility.matrix = {}
+
+utility.matrix.flip = matrix => {
+  return matrix[0].map((_, index) => (
+    matrix.map(row => row[index])
+  ))
+}
+
+utility.matrix.rotate90 = matrix => {
+  const flip = utility.matrix.flip,
+    reverse = utility.array.reverse
+
+  return flip(
+    reverse(matrix)
+  )
+}
+
+utility.matrix.rotate180 = matrix => {
+  const reverse = utility.array.reverse
+
+  return reverse(
+    matrix.map(row => reverse(row))
+  )
+}
+
+utility.matrix.rotate270 = matrix => {
+  const flip = utility.matrix.flip
+    reverse = utility.array.reverse
+
+  return reverse(
+    flip(matrix)
+  )
+}
