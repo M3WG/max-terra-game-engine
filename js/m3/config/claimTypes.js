d2cb29f37b4bf89c86a9fe2047be13b4de7c89ed
nicross <nicross@users.noreply.github.com>
2019-02-15 18:08:30 -0600

    Add shape priorities

--- js/m3/config/claimTypes.js
+++ js/m3/config/claimTypes.js
@@ -4,20 +4,22 @@ m3.config.claimTypes = {
   1: {
     id: 1,
     name: 'city',
+    priority: 1,
     shape: [
-        [
-          [1, 1],
-          [1, 1],
-        ],
-      ]
+      [
+        [1, 1],
+        [1, 1],
+      ],
+    ],
   },
   2: {
     id: 2,
     name: 'farmstead',
+    priority: 1,
     shape: [
       [
         [3, 3, 3],
-        [3, 0, 0]
+        [3, 0, 0],
       ],
       [
         [3, 0, 0],
@@ -28,6 +30,7 @@ m3.config.claimTypes = {
   3: {
     id: 3,
     name: 'logging camp',
+    priority: 1,
     shape: [
       [
         [4, 4, 0],
@@ -42,6 +45,7 @@ m3.config.claimTypes = {
   4: {
     id: 4,
     name: 'mine',
+    priority: 1,
     shape: [
       [
         [5, 5, 5],
