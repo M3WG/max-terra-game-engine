a39cdb541384cab04bfa9f75fb4f27f04dafcdab
nicross <nicross@users.noreply.github.com>
2019-02-18 23:03:55 -0600

    Create fn.js

---
+++ js/utility/fn.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var utility = utility || {}
+
+utility.fn = {}
+utility.fn.compose = (...fns) => x => fns.reduceRight((v, fn) => fn(v), x)
+utility.fn.identity = x => () => x
+utility.fn.pipe = (...fns) => x => fns.reduce((v, fn) => fn(v), x)
+utility.fn.unary = fn => x => fn(x)
