84c75f1e631c3e900f8b9bf6895ef178a8c7a59b
nicross <nicross@users.noreply.github.com>
2019-03-10 18:47:53 -0500

    Leverage claimTypes and shape definitions within m3.utility.match

--- js/m3/utility/match.js
+++ js/m3/utility/match.js
@@ -6,8 +6,8 @@ m3.utility.match = (action) => {
     cy = 3,
     slice = center.map.createSlice(center.getX() - 3, center.getY() - 3, 7, 7)
 
-  const is = (x, y, id) => {
-    const cell = slice.getCell(x, y)
+  const is = (dx, dy, id) => {
+    const cell = slice.getCell(cx + dx, cy + dy)
 
     if (!cell || cell.claim) {
       return false
@@ -16,293 +16,59 @@ m3.utility.match = (action) => {
     return cell.tile.getId() == id
   }
 
-  const test = (...tests) => tests.reduce((result, [id, x, y]) => result && is(x, y, id), true)
+  const evaluateTest = x => x.reduce((result, {dx, dy, tile}) => result && is(dx, dy, tile), true)
+  const gatherTestCells = x => x.map(({dx, dy}) => slice.getCell(cx + dx, cy + dy))
 
-  const createClaim = (type, ...cells) => m3.model.claim.create({
-    cell: cells.map(([id, x, y]) => slice.getCell(x, y)),
-    type: m3.model.claimType.createWithId(type),
-  })
+  for (const claimType of m3.model.claimType.getAll()) {
+    const shape = claimType.getShape()
 
-  /* test a city
-    AB
-    CD
-  */
-  //A
-  if (test(
-    [1, cx, cy],
-    [1, cx + 1, cy],
-    [1, cx, cy + 1],
-    [1, cx + 1, cy + 1]
-  )) {
-    return createClaim(
-      1,
-      [1, cx, cy],
-      [1, cx + 1, cy],
-      [1, cx, cy + 1],
-      [1, cx + 1, cy + 1]
-    )
-  }
-
-  //B
-  if (test(
-    [1, cx, cy],
-    [1, cx - 1, cy],
-    [1, cx, cy + 1],
-    [1, cx - 1, cy + 1]
-  )) {
-    return createClaim(
-      1,
-      [1, cx, cy],
-      [1, cx - 1, cy],
-      [1, cx, cy + 1],
-      [1, cx - 1, cy + 1]
-    )
-  }
-
-  //C
-  if (test(
-    [1, cx, cy],
-    [1, cx + 1, cy],
-    [1, cx, cy - 1],
-    [1, cx + 1, cy - 1]
-  )) {
-    return createClaim(
-      1,
-      [1, cx, cy],
-      [1, cx + 1, cy],
-      [1, cx, cy - 1],
-      [1, cx + 1, cy - 1]
-    )
-  }
-
-  //D
-  if (test(
-    [1, cx, cy],
-    [1, cx - 1, cy],
-    [1, cx, cy - 1],
-    [1, cx - 1, cy - 1]
-  )) {
-    return createClaim(
-      1,
-      [1, cx, cy],
-      [1, cx - 1, cy],
-      [1, cx, cy - 1],
-      [1, cx - 1, cy - 1]
-    )
-  }
-  //end of testing city shape
-
-  for (let rotation = 0; rotation < 4; rotation++) {
-
-    /* Mine tests  A
-                 B C D */
-    // A
-    if (test(
-      [5, cx, cy],
-      [5, cx - 1, cy + 1],
-      [5, cx, cy + 1],
-      [5, cx + 1, cy + 1]
-    )) {
-      return createClaim(
-        4,
-        [5, cx, cy],
-        [5, cx - 1, cy + 1],
-        [5, cx, cy + 1],
-        [5, cx + 1, cy + 1]
-      )
+    if (shape.rotate) {
+      for (let rotations = 0; rotations < 4; rotations++) {
+        if (shape.mirror) {
+          for (let mirrors = 0; mirrors < 2; mirrors++) {
+            for (const test of shape.test) {
+              if (evaluateTest(test)) {
+                return m3.model.claim.create({
+                  cell: gatherTestCells(test),
+                  type: claimType,
+                })
+              }
+            }
+            slice.flip()
+          }
+        } else {
+          for (const test of shape.test) {
+            if (evaluateTest(test)) {
+              return m3.model.claim.create({
+                cell: gatherTestCells(test),
+                type: claimType,
+              })
+            }
+          }
+        }
+        slice.rotate()
+      }
+    } else if (shape.mirror) {
+      for (let mirrors = 0; mirrors < 2; mirrors++) {
+        for (const test of shape.test) {
+          if (evaluateTest(test)) {
+            return m3.model.claim.create({
+              cell: gatherTestCells(test),
+              type: claimType,
+            })
+          }
+        }
+        slice.flip()
+      }
+    } else {
+      for (const test of shape.test) {
+        if (evaluateTest(test)) {
+          return m3.model.claim.create({
+            cell: gatherTestCells(test),
+            type: claimType,
+          })
+        }
+      }
     }
-
-    // B
-    if (test(
-      [5, cx, cy],
-      [5, cx + 1, cy - 1],
-      [5, cx + 1, cy],
-      [5, cx + 2, cy]
-    )) {
-      return createClaim(
-        4,
-        [5, cx, cy],
-        [5, cx + 1, cy - 1],
-        [5, cx + 1, cy],
-        [5, cx + 2, cy]
-      )
-    }
-
-    //C
-    if (test(
-      [5, cx, cy],
-      [5, cx, cy - 1],
-      [5, cx - 1, cy],
-      [5, cx + 1, cy]
-    )) {
-      return createClaim(
-        4,
-        [5, cx, cy],
-        [5, cx, cy - 1],
-        [5, cx - 1, cy],
-        [5, cx + 1, cy]
-      )
-    }
-
-    //D
-    if (test(
-      [5, cx, cy],
-      [5, cx - 1, cy],
-      [5, cx - 1, cy - 1],
-      [5, cx - 2, cy]
-    )) {
-      return createClaim(
-        4,
-        [5, cx, cy],
-        [5, cx - 1, cy],
-        [5, cx - 1, cy - 1],
-        [5, cx - 2, cy]
-      )
-    }
-    // end of testing mine shape
-
-    for (let flips = 0; flips < 2; flips++) {
-      /* Test if a farmstead A B C
-                             D
-      */
-      //A
-      if (test(
-        [3, cx, cy],
-        [3, cx + 1, cy],
-        [3, cx + 2, cy],
-        [3, cx, cy + 1]
-      )) {
-        return createClaim(
-          2,
-          [3, cx, cy],
-          [3, cx + 1, cy],
-          [3, cx + 2, cy],
-          [3, cx, cy + 1]
-        )
-      }
-
-      //B
-      if (test(
-        [3, cx, cy],
-        [3, cx - 1, cy],
-        [3, cx + 1, cy],
-        [3, cx - 1, cy + 1]
-      )) {
-        return createClaim(
-          2,
-          [3, cx, cy],
-          [3, cx - 1, cy],
-          [3, cx + 1, cy],
-          [3, cx - 1, cy + 1]
-        )
-      }
-
-      //C
-      if (test(
-        [3, cx, cy],
-        [3, cx - 1, cy],
-        [3, cx - 2, cy],
-        [3, cx - 2, cy + 1]
-      )) {
-        return createClaim(
-          2,
-          [3, cx, cy],
-          [3, cx - 1, cy],
-          [3, cx - 2, cy],
-          [3, cx - 2, cy + 1]
-        )
-      }
-
-      //D
-      if (test(
-        [3, cx, cy],
-        [3, cx, cy - 1],
-        [3, cx + 1, cy - 1],
-        [3, cx + 2, cy - 1]
-      )) {
-        return createClaim(
-          2,
-          [3, cx, cy],
-          [3, cx, cy - 1],
-          [3, cx + 1, cy - 1],
-          [3, cx + 2, cy - 1]
-        )
-      }
-      // end of testing farmstead shape
-
-      /* Test if a logging camp A B
-                                  C D
-      */
-      //A
-      if (test(
-        [4, cx, cy],
-        [4, cx + 1, cy],
-        [4, cx + 1, cy + 1],
-        [4, cx + 2, cy + 1]
-      )) {
-        return createClaim(
-          3,
-          [4, cx, cy],
-          [4, cx + 1, cy],
-          [4, cx + 1, cy + 1],
-          [4, cx + 2, cy + 1]
-        )
-      }
-
-      //B
-      if (test(
-        [4, cx, cy],
-        [4, cx - 1, cy],
-        [4, cx, cy + 1],
-        [4, cx + 1, cy + 1]
-      )) {
-        return createClaim(
-          3,
-          [4, cx, cy],
-          [4, cx - 1, cy],
-          [4, cx, cy + 1],
-          [4, cx + 1, cy + 1]
-        )
-      }
-
-      //C
-      if (test(
-        [4, cx, cy],
-        [4, cx - 1, cy - 1],
-        [4, cx, cy - 1],
-        [4, cx + 1, cy]
-      )) {
-        return createClaim(
-          3,
-          [4, cx, cy],
-          [4, cx - 1, cy - 1],
-          [4, cx, cy - 1],
-          [4, cx + 1, cy]
-        )
-      }
-
-      //D
-      if (test(
-        [4, cx, cy],
-        [4, cx - 2, cy - 1],
-        [4, cx - 1, cy - 1],
-        [4, cx - 1, cy]
-      )) {
-        return createClaim(
-          3,
-          [4, cx, cy],
-          [4, cx - 2, cy - 1],
-          [4, cx - 1, cy - 1],
-          [4, cx - 1, cy]
-        )
-      }
-      // end of testing logging camp shape
-
-      // End of flips loop. Flip slice.
-      slice.flip()
-    }
-
-    // End of tests loop. Rotate slice.
-    slice.rotate()
   }
 }
