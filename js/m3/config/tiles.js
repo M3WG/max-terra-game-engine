102c3d294429901336af38154bf232480bb8e22e
nicross <nicross@users.noreply.github.com>
2019-04-21 23:40:37 -0500

    Make picker more configurable

--- js/m3/config/tiles.js
+++ js/m3/config/tiles.js
@@ -6,47 +6,76 @@ m3.config.tiles = {
     color: '#000000',
     desc: 'void',
     icon: '&nbsp;',
+    picker: {},
   },
   1: {
     id: 1,
     color: '#FFFFFF',
     desc: 'house',
     icon: '👪',
+    picker: {
+      active: true,
+      position: 0,
+    },
   },
   2: {
     id: 2,
     color: '#4146db',
     desc: 'water',
     icon: '🌊',
+    picker: {
+      active: true,
+      position: 6,
+    },
   },
   3: {
     id: 3,
     color: '#edde6f',
     desc: 'field',
     icon: '🌻',
+    picker: {
+      active: true,
+      position: 1,
+    },
   },
   4: {
     id: 4,
     color: '#2d873e',
     desc: 'forest',
     icon: '🌲',
+    picker: {
+      active: true,
+      position: 2,
+    },
   },
   5: {
     id: 5,
     color: '#555555',
     desc: 'stone',
     icon: '⛰️',
+    picker: {
+      active: true,
+      position: 3,
+    },
   },
   6: {
     id: 6,
     color: '#FF9900',
     desc: 'gold',
     icon: '💎',
+    picker: {
+      active: true,
+      position: 4,
+    },
   },
   7: {
     id: 7,
     color: '#FF00FF',
     desc: 'magic',
     icon: '🧙',
+    picker: {
+      active: true,
+      position: 5,
+    },
   },
 }
