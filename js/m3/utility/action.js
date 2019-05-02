d27f5bad7225e8201c0d5f52d3fa44e2b5649bba
nicross <nicross@users.noreply.github.com>
2019-05-01 19:39:27 -0500

    Cleanup m3.utility.action.validate()

--- js/m3/utility/action.js
+++ js/m3/utility/action.js
@@ -3,21 +3,27 @@
 m3.utility.action = {}
 
 m3.utility.action.validate = (options) => {
-  const cell = options.cell
+  const cell = options.cell,
+    cellTile = cell.tile,
+    tile = options.tile
 
   if (!m3.model.cell.prototype.isPrototypeOf(cell)) {
     throw new Error('Please provide a cell')
   }
 
-  if (cell.claim) {
-    throw new Error('Cell is already claimed')
+  if (!m3.model.tile.prototype.isPrototypeOf(tile)) {
+    throw new Error('Please provide a tile')
   }
 
-  if (cell.tile.getId() == 2) {
+  if (cell.claim) {
+    throw new Error('Cell is claimed')
+  }
+
+  if (cellTile.getId() == 2) {
     throw new Error('Cell contains water')
   }
 
-  if (cell.tile.getId() == options.tile.getId()) {
+  if (cellTile.getId() == tile.getId()) {
     throw new Error('Cell cannot be changed to itself')
   }
 
