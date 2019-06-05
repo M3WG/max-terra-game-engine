48a65c38fda204db80ea5d2393bf2a43f5eafacf
nicross <nicross@users.noreply.github.com>
2019-06-04 21:14:30 -0500

    Add getSimilarCells() and getSimilarCellsGreedy() to m3.utility.adjacency

--- js/m3/utility/adjacency.js
+++ js/m3/utility/adjacency.js
@@ -8,6 +8,40 @@ m3.utility.adjacency.getCells = (
   }
 )()
 
+m3.utility.adjacency.getSimilarCells = (
+  () => {
+    const crawler = m3.utility.crawler.create(),
+      isSameType = (cell) => (test) => cell.type === test.type
+
+    return (cell) => crawler.initializeWithCell(cell).getAdjacent().filter(isSameType(cell))
+  }
+)()
+
+m3.utility.adjacency.getSimilarCellsGreedy = (cell) => {
+  const cells = m3.utility.adjacency.getSimilarCells(cell),
+    tested = []
+
+  let more = true
+
+  while (more) {
+    more = false
+    cells.forEach((cell) => {
+      if (tested.includes(cell)) {
+        return
+      }
+
+      m3.utility.adjacency.getSimilarCells(cell).forEach((cell) => {
+        if (!cells.includes(cell)) {
+          cells.push(cell)
+          more = true
+        }
+      })
+    })
+  }
+
+  return claims
+}
+
 m3.utility.adjacency.getClaims = (claim) => {
   if (!m3.model.claim.prototype.isPrototypeOf(claim)) {
     throw new Error('Please provide a valid claim')
