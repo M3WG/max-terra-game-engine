b9d4b7aebcd9b62b4ca50f8276d1ec9e79346cf3
nicross <nicross@users.noreply.github.com>
2019-06-21 23:16:30 -0500

    Implement m3.model.path

--- js/m3/model/path.js
+++ js/m3/model/path.js
@@ -9,6 +9,10 @@ m3.model.path.prototype = (
     function construct(...args) {
       _prototype.construct.call(this, ...args)
 
+      if (!Array.isArray(this.config.cell)) {
+        this.config.cell = []
+      }
+
       return this
     }
 
@@ -16,11 +20,33 @@ m3.model.path.prototype = (
       return this
     }
 
-    // Cell[]
+    function combine(...paths) {
+      if (!m3.model.path.prototype.isPrototypeOf(path)) {
+        return this
+      }
+
+      return m3.model.path.create({
+        cell: paths.reduce((cells, path) => [
+          ...cells,
+          ...path.getCells()
+        ], this.getCells()),
+      })
+    }
+
+    function getCells() {
+      return utility.array.copy(this.config.cell)
+    }
+
+    function getSize() {
+      return this.config.cell.length
+    }
 
     return Object.setPrototypeOf({
       construct,
       destruct,
+      combine,
+      getCells,
+      getSize,
     }, _prototype)
   }
 )()
