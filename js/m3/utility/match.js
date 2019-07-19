785ecfc1fdc4f8e46b04f06a27b37ee74069d4f7
nicross <nicross@users.noreply.github.com>
2019-07-19 17:43:29 -0500

    Allow wizard towers to be merged

--- js/m3/utility/match.js
+++ js/m3/utility/match.js
@@ -9,17 +9,12 @@ m3.utility.match = (action, player) => {
 
   // XXX: Special case for single magic tile to tower
   // TODO: Make configurable the number of cells needed for match
-  if (actionTile.getId() == 7) {
-    return {
-      cell: [action.cell],
-      type: m3.model.claimType.get(6),
-    }
-  }
+  const minimumCells = actionTile.getId() == 7 ? 0 : 3
 
   const canMatch = (cell) => !cell.claim || cell.claim && cell.claim.player == player
   const cells = m3.utility.adjacency.getSimilarCellsGreedy(action.cell, canMatch)
 
-  if (cells.length > 3) {
+  if (cells.length > minimumCells) {
     return {
       cell: cells,
       type: actionTile.getClaimType(),
