fbd305497bd0a1cb698bf2dcaaee4d619cd93327
nicross <nicross@users.noreply.github.com>
2019-07-27 13:51:46 -0500

    Remove paths (as we know it)

--- js/m3/utility/path.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-m3.utility.path = {}
-
-m3.utility.path.magicWand = function (cell, filter) {
-  return this.create({
-    cell: m3.utility.adjacency.getSimilarCellsGreedy(cell, filter),
-  })
-}
