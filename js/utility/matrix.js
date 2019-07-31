7aae910c3c7d250058713585fe522384b7977ebe
nicross <nicross@users.noreply.github.com>
2019-07-31 00:12:09 -0500

    Update matrix.js

--- js/utility/matrix.js
+++ js/utility/matrix.js
@@ -4,12 +4,12 @@ var utility = utility || {}
 
 utility.matrix = {}
 
-utility.matrix.flip = matrix => (
+utility.matrix.flip = (matrix) => (
   matrix[0].map((_, index) => (
     matrix.map(row => row[index])
   ))
 )
 
 utility.matrix.rotate90 = utility.fn.pipe(utility.array.reverse, utility.matrix.flip)
-utility.matrix.rotate180 = matrix => utility.array.reverse(matrix.map(utility.array.reverse))
+utility.matrix.rotate180 = (matrix) => utility.array.reverse(matrix.map(utility.array.reverse))
 utility.matrix.rotate270 = utility.fn.pipe(utility.matrix.flip, utility.array.reverse)
