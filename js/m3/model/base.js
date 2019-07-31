d31f85a3bd3a7ba6aa300d51364afa38faaf494b
nicross <nicross@users.noreply.github.com>
2019-07-31 18:42:27 -0500

    Improve model lifecycle

--- js/m3/model/base.js
+++ js/m3/model/base.js
@@ -2,13 +2,26 @@
 
 m3.model.base = {}
 
+m3.model.base.is = (x) => m3.model.base.isPrototypeOf(x)
+
 m3.model.base.prototype = (
   (undefined) => {
 
     function construct(config) {
+      // TODO: Rethink config
       this.config = Object.assign({}, config)
 
       utility.pubsub.decorate(this)
+
+      this.setup()
+
+      return this
+    }
+
+    function destroy() {
+      this.config = {}
+
+      this.teardown()
 
       return this
     }
@@ -17,9 +30,20 @@ m3.model.base.prototype = (
       return Number(this.config.id)
     }
 
+    function setup() {
+      return this
+    }
+
+    function teardown() {
+      return this
+    }
+
     return {
       construct,
+      destroy,
       getId,
+      setup,
+      teardown,
     }
   }
 )()
