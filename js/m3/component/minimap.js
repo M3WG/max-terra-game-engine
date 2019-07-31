2600074354729fd01b45675baa13cd453d95809c
nicross <nicross@users.noreply.github.com>
2019-07-31 00:46:56 -0500

    Update minimap.js

--- js/m3/component/minimap.js
+++ js/m3/component/minimap.js
@@ -1,37 +1,15 @@
 'use strict'
 
-m3.component.minimap = {}
+m3.component.minimap = m3.utility.component.inventFactory(
+ ((undefined) => {
 
-m3.component.minimap.prototype = (
-  (undefined) => {
-    const _prototype = m3.component.base.prototype
-
-    function construct(...args) {
-      _prototype.construct.call(this, ...args)
-
-      _build.call(this)
-
-      this.attach()
-
-      return this
-    }
-
-    function getModel() {
-      return this.config.model
-    }
-
-    function _build() {
+    function setup() {
       this._rootElement = document.createElement('div')
       this._rootElement.className = 'm3-c-minimap'
     }
 
-    return Object.setPrototypeOf({
-      construct,
-      getModel,
-    }, _prototype)
-  }
-)()
-
-m3.component.minimap.create = function create(...args) {
-  return Object.create(this.prototype).construct(...args)
-}
+    return {
+      setup,
+    }
+  })()
+)
