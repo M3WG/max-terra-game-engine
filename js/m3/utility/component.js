2e66cdb99a4203daa72b667134f656494083a303
nicross <nicross@users.noreply.github.com>
2019-07-30 23:59:04 -0500

    Create m3.utility.component

---
+++ js/m3/utility/component.js
@@ -0,0 +1,17 @@
+'use strict'
+
+m3.utility.component = {}
+
+m3.utility.component.factory = (prototype, mixin = {}) => ({
+  create: function create(...args) {
+    return Object.create(this.prototype).construct(...args)
+  },
+  is: function is(x) {
+    return this.prototype.isPrototypeOf(x)
+  },
+  prototype,
+  ...mixin,
+})
+
+m3.utility.component.invent = (definition) => Object.setPrototypeOf(Object.assign({}, definition), m3.component.base)
+m3.utility.component.inventFactory = (definition, mixin) => m3.utility.component.factory(m3.utility.component.invent(definition), mixin)
