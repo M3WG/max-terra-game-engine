ffacea6b35fa45686d7b5fb6ff641db317d13838
nicross <nicross@users.noreply.github.com>
2019-07-31 23:31:14 -0500

    Add m3.utility.model.singletonFactory / inventSingletonFactory

--- js/m3/utility/model.js
+++ js/m3/utility/model.js
@@ -13,5 +13,37 @@ m3.utility.model.factory = (prototype, mixin = {}) => ({
   ...mixin,
 })
 
+m3.utility.model.singletonFactory = (prototype, mixin = {}) => m3.utility.model.factory(prototype, {
+  data: {},
+  get: function (id) {
+    if (this.is(id)) {
+      return id
+    }
+
+    if (this.store.has(id)) {
+      return this.store.get(id)
+    }
+
+    const data = this.data[id]
+    data.id = id
+
+    const instance = this.create(data)
+    this.store.set(id, instance)
+    return instance
+  },
+  getSome: function(ids) {
+    return ids.reduce((models, id) => {
+      models.push(this.get(id))
+      return models
+    }, [])
+  },
+  getAll: function() {
+    return Object.keys(this.data).map((id) => this.get(id))
+  },
+  store: new Map(),
+  ...mixin,
+})
+
 m3.utility.model.invent = (definition) => Object.setPrototypeOf(Object.assign({}, definition), m3.model.base.prototype)
 m3.utility.model.inventFactory = (definition, mixin) => m3.utility.model.factory(m3.utility.model.invent(definition), mixin)
+m3.utility.model.inventSingletonFactory = (definition, mixin) => m3.utility.model.singletonFactory(m3.utility.model.invent(definition), mixin)
