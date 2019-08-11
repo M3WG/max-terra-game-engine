1942b216c790a57093b62d9042c70eddb2093bbb
nicross <nicross@users.noreply.github.com>
2019-08-11 16:10:59 -0500

    Return pure functions via utility.fn.identity

--- js/utility/fn.js
+++ js/utility/fn.js
@@ -5,7 +5,7 @@ var utility = utility || {}
 utility.fn = {
   const: (x) => () => x,
   compose: (...fns) => (x) => fns.reduceRight((v, fn) => fn(v), x),
-  identity: (x) => x,
+  identity: () => (x) => x,
   pipe: (...fns) => (x) => fns.reduce((v, fn) => fn(v), x),
   unary: (fn) => (x) => fn(x),
 }
