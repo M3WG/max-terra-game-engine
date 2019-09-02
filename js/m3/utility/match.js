52309aa9f2d31dd318c0e1ca285e3aafe205250d
nicross <nicross@users.noreply.github.com>
2019-09-02 13:58:53 -0500

    Resolve tasks and improve m3.utility.match.shape()

--- js/m3/utility/match.js
+++ js/m3/utility/match.js
@@ -2,12 +2,15 @@
 
 m3.utility.match = {}
 
-// TODO: Define shapes struct
-// SEE: https://github.com/M3WG/m3-prototype/blob/3af76bb23434fb8feb5ae151e665c4e10eb0526c/js/m3/config/shapes.js
-m3.utility.match.shape = (cell, shapes, filter) => {
-  // XXX: Slice has hardcoded dimensions (7x7)
-  // TODO: Make this configurable or automaticaLly derived from shapes
-  const slice = center.getMap().createSlice(cell.getX() - 3, cell.getY() - 3, 7, 7),
+// TODO: Define definition struct
+// SEE: example/tic-tac-toe
+m3.utility.match.shape = (cell, {definition = [], mirror = false, rotate = false}, filter) => {
+  const radius = definition.reduce((max, {dx, dy}) => {
+    return Math.max(max, Math.abs(dx), Math.abs(dy))
+  }, 0)
+
+  const diameter = radius * 2 + 1,
+    slice = cell.getMap().createSlice(cell.getX() - radius, cell.getY() - radius, diameter, diameter),
     {x: cx, y: cy} = slice.getCellCoordinates(cell)
 
   if (typeof filter != 'function') {
@@ -18,28 +21,48 @@ m3.utility.match.shape = (cell, shapes, filter) => {
     filter = (x) => x
   }
 
-  // TODO: buildPermutations() for DX so shapes only need one defined permutation (e.g. top-left)
+  const permutations = [
+    definition,
+  ]
+
+  for (let i = 1, length = definition.length; i < length; i++) {
+    const {dx, dy} = definition[i]
+    const translate = (cell) => ({dx: cell.dx - dx, dy: cell.dy - dy})
+
+    permutations.push(
+      definition.map(translate)
+    )
+  }
+
   const gatherPermutation = cells => cells.map(({dx, dy}) => slice.getCell(cx + dx, cy + dy))
   const testPermutation = cells => cells.reduce((result, {dx, dy}) => result && filter(slice.getCell(cx + dx, cy + dy)), true)
 
-  for (const shape of shapes) {
-    let rotations = shape.rotate ? 4 : 0
+  let rotations = rotate ? 4 : 0
+  do {
+    let mirrors = mirror ? 2 : 0
     do {
-      let mirrors = shape.mirror ? 2 : 0
-      do {
-        for (const permutation of shape.permutation) {
-          if (testPermutation(permutation)) {
-            return gatherPermutation(permutation)
-          }
+      for (const permutation of permutations) {
+        if (testPermutation(permutation)) {
+          return gatherPermutation(permutation)
         }
-        if (mirrors > 0) {
-          slice.flip()
-        }
-      } while (mirrors-->1)
-
-      if (rotations > 0) {
-        slice.rotate()
       }
-    } while (rotations-->1)
+      if (mirrors > 0) {
+        slice.flip()
+      }
+    } while (mirrors-->1)
+
+    if (rotations > 0) {
+      slice.rotate()
+    }
+  } while (rotations-->1)
+}
+
+m3.utility.match.shapes = (cell, shapes, filter) => {
+  for (const shape of shapes) {
+    const result = m3.utility.match.shape(cell, shape, filter)
+
+    if (result) {
+      return result
+    }
   }
 }
