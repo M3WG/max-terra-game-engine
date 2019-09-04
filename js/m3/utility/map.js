fcab2971a737b578b8a7585b63d70d38f9db4059
nicross <nicross@users.noreply.github.com>
2019-09-04 00:15:56 -0500

    Update map.js

--- js/m3/utility/map.js
+++ js/m3/utility/map.js
@@ -1,17 +1,20 @@
 'use strict'
 
 /**
- * Utilities for working with {@link m3.model.map} instances.
+ * Utilities for working with {@link m3.model.map.prototype} instances.
  *
  * @namespace m3.utility.map
  */
 m3.utility.map = {}
 
 /**
- * Fills the model's cells with the specified tile.
+ * Fills the map's cells with the specified tile.
+ *
+ * @param {m3.model.map.prototype} map
+ * @param {m3.model.tile.prototype} tile
  */
-m3.utility.map.fill = (model, tile) => {
-  model.getCells().forEach((cell) => {
+m3.utility.map.fill = (map, tile) => {
+  map.getCells().forEach((cell) => {
     cell.setTile(tile)
   })
 
@@ -19,23 +22,28 @@ m3.utility.map.fill = (model, tile) => {
 }
 
 /**
- * Returns the percentage of the model's cells that pass a filter function.
+ * Returns the percentage of the map's cells that pass a filter function.
+ *
+ * @param {m3.model.map.prototype} map
+ * @param {function} filter
  */
-m3.utility.map.getPercent = (model, filter) => {
+m3.utility.map.getPercent = (map, filter) => {
   if (typeof filter != 'function') {
     filter = utility.fn.identity()
   }
 
-  const cells = model.getCells()
+  const cells = map.getCells()
   return cells.filter(filter).length / cells.length
 }
 
 /**
- * Imports a two-dimensional array of tiles into the model's cells.
- * Arrays must be in the format of `[y][x]`.
+ * Imports a two-dimensional array of tiles into the map's cells.
+ *
+ * @param {m3.model.map.prototype} map
+ * @param {m3.model.tile.prototype[][]} data - In the format of `[y][x]`
  */
-m3.utility.map.import = (model, data) => {
-  model.getCells().forEach((cell) => {
+m3.utility.map.import = (map, data) => {
+  map.getCells().forEach((cell) => {
     const x = cell.getX(),
       y = cell.getY()
 
@@ -44,10 +52,12 @@ m3.utility.map.import = (model, data) => {
 }
 
 /**
- * Randomly fills the model's cells with tiles having a randomWeight property.
+ * Randomly fills the map's cells with tiles having a randomWeight property.
  * Higher weights equal higher probabilities to appear.
+ *
+ * @param {m3.model.map.prototype} map
  */
-m3.utility.map.randomize = (model) => {
+m3.utility.map.randomize = (map) => {
   const tiles = utility.array.shuffle(
     m3.model.tile.getAll().reduce((tiles, tile) => [
       ...tiles,
@@ -55,7 +65,7 @@ m3.utility.map.randomize = (model) => {
     ], [])
   )
 
-  model.getCells().forEach((cell) => cell.setTile(
+  map.getCells().forEach((cell) => cell.setTile(
     utility.array.randomValue(tiles)
   ))
 }
