99adcbd89a7634d60f9446295fb04abd01e6e328
nicross <nicross@users.noreply.github.com>
2019-09-03 23:20:43 -0500

    Document base component and model

--- js/m3/component/base.js
+++ js/m3/component/base.js
@@ -1,8 +1,24 @@
 'use strict'
 
+/**
+ * Namespace for the base component that all components extend.
+ * There is no factory here because it's intended to be abstract.
+ *
+ * @namespace m3.component.base
+ */
 m3.component.base = {}
 
+/**
+ * Prototype for the base component.
+ *
+ * @borrows utility.pubsub.prototype.emit
+ * @borrows utility.pubsub.prototype.off
+ * @borrows utility.pubsub.prototype.on
+ */
 m3.component.base.prototype = {
+  /**
+   * Appends the root element to the specified Node.
+   */
   attach: function (node) {
     if (node instanceof Node) {
       node.appendChild(this.rootElement)
@@ -10,6 +26,13 @@ m3.component.base.prototype = {
 
     return this
   },
+  /**
+   * Called when creating a new instance, typically automatically via a factory.
+   * Components must not override this method.
+   *
+   * @final
+   * @see {@link m3.component.base.setup}
+   */
   create: function ({parentComponent, parentElement, ...config}, ...args) {
     this.config = config
     this.parent = parentComponent
@@ -19,6 +42,13 @@ m3.component.base.prototype = {
 
     return this
   },
+  /**
+   * Prepares the component for garbage collection.
+   * Components must not override this method.
+   *
+   * @final
+   * @see {@link m3.component.base.teardown}
+   */
   destroy: function () {
     this.teardown()
     this.pubsub.destroy()
@@ -27,6 +57,9 @@ m3.component.base.prototype = {
 
     return this
   },
+  /**
+   * Removes the root element from the DOM until attached again.
+   */
   detach: function (node) {
     if (this.rootElement && this.rootElement.parentNode instanceof Node) {
       this.rootElement.parentNode.removeChild(this.rootElement)
@@ -34,6 +67,11 @@ m3.component.base.prototype = {
 
     return this
   },
+  /**
+   * Returns the dimensions and position of the root element relative to the viewport.
+   *
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect}
+   */
   getBoundingClientRect: function () {
     if (!(this.rootElement instanceof Element)) {
       return {}
@@ -41,9 +79,20 @@ m3.component.base.prototype = {
 
     return this.rootElement.getBoundingClientRect()
   },
+  /**
+   * Called via {@link app.component.base.create}.
+   * Components must override this method to create and assign an element to `this.rootElement`.
+   *
+   * @abstract
+   * @param {...*} - Additional arguments from {@link app.component.base.create}
+   */
   setup: function () {
     return this
   },
+  /**
+   * Called via {@link m3.component.base.destroy}.
+   * Components should override this method to free any refrences for garbage collection.
+   */
   teardown: function () {
     return this
   },
--- js/m3/model/base.js
+++ js/m3/model/base.js
@@ -1,8 +1,30 @@
 'use strict'
 
+/**
+ * Namespace for the base model that all models extend.
+ * There is no factory here because it's intended to be abstract.
+ * @namespace m3.model.base
+ */
 m3.model.base = {}
 
+/**
+ * Prototype for the base model.
+ *
+ * The base model provides a simple wrapper for a flat internal data structure.
+ * Users are encouraged to extend it with custom members with getters and setters.
+ *
+ * @borrows utility.pubsub.prototype.emit
+ * @borrows utility.pubsub.prototype.off
+ * @borrows utility.pubsub.prototype.on
+ */
 m3.model.base.prototype = {
+  /**
+   * Called when instantiating a new instance, typically automatically via a factory.
+   * Models must not override this method.
+   *
+   * @final
+   * @see {@link m3.model.base.setup}
+   */
   create: function (data, ...args) {
     this.data = {...data}
 
@@ -11,35 +33,71 @@ m3.model.base.prototype = {
 
     return this
   },
+  /**
+   * Prepares the model for garbage collection.
+   * Models must not override this method.
+   *
+   * @final
+   * @see {@link m3.model.base.teardown}
+   */
   destroy: function () {
     this.teardown()
+    this.pubsub.destroy()
+
     this.data = {}
 
     return this
   },
+  /**
+   * Returns whether `key` exists within the internal data structure.
+   */
   has: function(key) {
     return typeof this.data[key] != 'undefined'
   },
+  /**
+   * Returns the value for `key` within the internal data structure.
+   */
   get: function(key) {
     return this.data[key]
   },
+  /**
+   * Returns the identifier.
+   * Shorthand for `this.get('id')`.
+   */
   getId: function () {
     return this.data.id
   },
+  /**
+   * Returns whether `key` is equal to `value` within the internal data structure.
+   */
   is: function (key, value, strict) {
     if (strict) {
       return this.data[key] === value
     }
+
     return this.data[key] == value
   },
+  /**
+   * Sets `key` to `value` within the internal data structure.
+   */
   set: function (key, value) {
     this.data[key] = value
     this.emit('change')
     return this
   },
+  /**
+   * Called via {@link app.model.base.create}.
+   * Models can override this method to initialize custom members.
+   *
+   * @param {...*} - Additional arguments from {@link app.model.base.create}
+   */
   setup: function () {
     return this
   },
+  /**
+   * Called via {@link m3.model.base.destroy}.
+   * Models should override this method to free any refrences for garbage collection.
+   */
   teardown: function () {
     return this
   }
