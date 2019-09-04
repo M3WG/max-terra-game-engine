9d1e5de6fcbefaa2ac1f4ab7e3a930ae29e8a0cc
nicross <nicross@users.noreply.github.com>
2019-09-03 23:33:32 -0500

    Document m3.utility.map

--- js/m3/utility/map.js
+++ js/m3/utility/map.js
@@ -1,13 +1,26 @@
 'use strict'
 
+/**
+ * Utilities for working with {@link m3.model.map} instances.
+ *
+ * @namespace m3.utility.map
+ */
 m3.utility.map = {}
 
+/**
+ * Fills the model's cells with the specified tile.
+ */
 m3.utility.map.fill = (model, tile) => {
   model.getCells().forEach((cell) => {
     cell.setTile(tile)
   })
+
+  return this
 }
 
+/**
+ * Returns the percentage of the model's cells that pass a filter function.
+ */
 m3.utility.map.getPercent = (model, filter) => {
   if (typeof filter != 'function') {
     filter = utility.fn.identity()
@@ -17,6 +30,10 @@ m3.utility.map.getPercent = (model, filter) => {
   return cells.filter(filter).length / cells.length
 }
 
+/**
+ * Imports a two-dimensional array of tiles into the model's cells.
+ * Arrays must be in the format of `[y][x]`.
+ */
 m3.utility.map.import = (model, data) => {
   model.getCells().forEach((cell) => {
     const x = cell.getX(),
@@ -26,6 +43,10 @@ m3.utility.map.import = (model, data) => {
   })
 }
 
+/**
+ * Randomly fills the model's cells with tiles having a randomWeight property.
+ * Higher weights equal higher probabilities to appear.
+ */
 m3.utility.map.randomize = (model) => {
   const tiles = utility.array.shuffle(
     m3.model.tile.getAll().reduce((tiles, tile) => [
