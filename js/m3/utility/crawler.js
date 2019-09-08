c5e252512435ae47e63d4e70ab190c9c7a90a8d8
nicross <nicross@users.noreply.github.com>
2019-09-08 11:51:28 -0500

    Reformat and document m3.utility.crawler

--- js/m3/utility/crawler.js
+++ js/m3/utility/crawler.js
@@ -1,146 +1,105 @@
 'use strict'
 
+/**
+ * Provides a natural language interface for navigating a map.
+ *
+ * @namespace m3.utility.crawler
+ */
 m3.utility.crawler = {}
 
-m3.utility.crawler.create = (...args) => Object.create(m3.utility.crawler.prototype).create(...args)
+/**
+ * Creates an instance of {@link m3.utility.crawler.prototype}.
+ */
+m3.utility.crawler.create = function (...args) {
+  return Object.create(this.prototype).initialize(...args)
+}
 
-m3.utility.crawler.prototype = (
-  (undefined) => {
+/**
+ * Prototype for the crawler interface.
+ */
+m3.utility.crawler.prototype = {
+  getAdjacent: function () {
+    return [
+      this.getDown(),
+      this.getLeft(),
+      this.getRight(),
+      this.getUp(),
+    ].filter((cell) => m3.model.cell.is(cell))
+  },
+  getDown: function () {
+    const x = this.getX(),
+      y = this.getY() - 1
 
-    function create({cell, map, x, y}) {
-      if (m3.model.cell.is(cell)) {
-        return this.initializeWithCell(cell)
-      }
+    return this.getMap().getCell(x, y)
+  },
+  getLeft: function () {
+    const x = this.getX() - 1,
+      y = this.getY()
 
-      if (map) {
-        this.setMap(map)
-      }
+    return this.getMap().getCell(x, y)
+  },
+  getMap: function () {
+    return this.map
+  },
+  getRight: function () {
+    const x = this.getX() + 1,
+      y = this.getY()
 
-      this.moveTo(x, y)
+    return this.getMap().getCell(x, y)
+  },
+  getUp: function () {
+    const x = this.getX(),
+      y = this.getY() + 1
 
-      return this
-    }
-
-    function destruct() {
-      return this
-    }
-
-    function getAdjacent() {
-      return [
-        this.getDown(),
-        this.getLeft(),
-        this.getRight(),
-        this.getUp(),
-      ].filter((cell) => m3.model.cell.is(cell))
-    }
-
-    function getDown() {
-      const x = this.getX(),
-        y = this.getY() - 1
-
-      return this.map.getCell(x, y)
-    }
-
-    function getLeft() {
-      const x = this.getX() - 1,
-        y = this.getY()
-
-      return this.map.getCell(x, y)
-    }
-
-    function getRight() {
-      const x = this.getX() + 1,
-        y = this.getY()
-
-      return this.map.getCell(x, y)
-    }
-
-    function getUp() {
-      const x = this.getX(),
-        y = this.getY() + 1
-
-      return this.map.getCell(x, y)
-    }
-
-    function getX() {
-      return this._x;
-    }
-
-    function getY() {
-      return this._y;
-    }
-
-    function initializeWithCell(cell) {
-      if (!m3.model.cell.is(cell)) {
-        throw new Error('Please provide a valid cell')
-      }
-
+    return this.getMap().getCell(x, y)
+  },
+  getX: function () {
+    return this.x;
+  },
+  getY: function () {
+    return this.y;
+  },
+  initialize: function ({cell, map, x, y}) {
+    if (m3.model.cell.is(cell)) {
       return this
         .setMap(cell.getMap())
         .moveTo(cell.getX(), cell.getY())
     }
 
-    function moveDown() {
-      return this.setY(this.getY() + 1)
+    this.setMap(map)
+    this.moveTo(x, y)
+
+    return this
+  },
+  moveDown: function () {
+    return this.setY(this.getY() + 1)
+  },
+  moveLeft: function () {
+    return this.setX(this.getX() - 1)
+  },
+  moveRight: function () {
+    return this.setX(this.getX() + 1)
+  },
+  moveTo: function (x, y) {
+    return this.setX(x).setY(y)
+  },
+  moveUp: function () {
+    return this.setY(this.getY() - 1)
+  },
+  setMap: function (map) {
+    if (!m3.model.map.is(map)) {
+      throw new Error('Please provide a valid map')
     }
 
-    function moveLeft() {
-      return this.setX(this.getX() - 1)
-    }
-
-    function moveRight() {
-      return this.setX(this.getX() + 1)
-    }
-
-    function moveTo(x, y) {
-      return this.setX(x).setY(y)
-    }
-
-    function moveUp() {
-      return this.setY(this.getY() - 1)
-    }
-
-    function setMap(map) {
-      if (!m3.model.map.is(map)) {
-        throw new Error('Please provide a valid map')
-      }
-
-      this.map = map
-
-      return this
-    }
-
-    function setX(x) {
-      this._x = Number(x) || 0
-
-      return this
-    }
-
-    function setY(y) {
-      this._y = Number(y) || 0
-
-      return this
-    }
-
-    return {
-      create,
-      destruct,
-      getAdjacent,
-      getDown,
-      getLeft,
-      getRight,
-      getUp,
-      getX,
-      getY,
-      initializeWithCell,
-      moveDown,
-      moveLeft,
-      moveRight,
-      moveTo,
-      moveUp,
-      setMap,
-      setX,
-      setY,
-    }
-  }
-)()
+    this.map = map
+    return this
+  },
+  setX: function (x) {
+    this.x = Number(x) || 0
+    return this
+  },
+  setY: function (y) {
+    this.y = Number(y) || 0
+    return this
+  },
+}
